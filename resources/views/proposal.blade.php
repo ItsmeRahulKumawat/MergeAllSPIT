69b3864e17483a7de1f8cd3c118700e0b8d58973
Shubham <25881429+shoebham@users.noreply.github.com>
2022-11-03 20:12:21 +0530

    create form

---
+++ resources/views/proposal.blade.php
@@ -0,0 +1,44 @@
+
+<html>
+    <head>
+        <title>Proposal</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+    <form>
+  <div class="form-group">
+    <label for="title-proposal">Title of Proposal</label>
+    <input type="text" class="form-control" id="title-proposal" placeholder="Title of Proposal">
+  </div>
+  <div class="form-group">
+    <label for="authority">Authority/Organization</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Authority</option>
+      <option>Organization</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="govt">Govt./Private</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Govt</option>
+      <option>Private</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="abstract">Abstract</label>
+    <textarea class="form-control" id="abstract" rows="3"></textarea>
+  </div>
+  <div class="form-group">
+    <label for="amount">Funding amount applied for</label>
+    <input type="Number" class="form-control" id="amount" placeholder="Funding amount">
+  </div>
+  <div class="form-group">
+    <label for="amount">Funding amount applied for</label>
+    <input type="date" class="form-control" id="amount" placeholder="Funding amount">
+  </div>
+</form>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
     return view('homepage');
 });
 
-
+Route::get('/proposal',function(){
+    return view('proposal');
+});
