65102e07f8e257867aa46b639e2f535ae40e0f37
Shubham <25881429+shoebham@users.noreply.github.com>
2023-01-05 15:54:38 +0530

    fix storage

--- resources/views/proposal_details.blade.php
+++ resources/views/proposal_details.blade.php
@@ -11,9 +11,9 @@
     
     {{-- if proposal is set --}}
     @if (isset($proposal))
-        <iframe src="{{ asset('storage/'.$proposal->proposal_file) }}" width="100%" height="1000px"></iframe>
+        <iframe src="{{ asset('proposal_outreach/storage/'.$proposal->proposal_file) }}" width="100%" height="1000px"></iframe>
     @else
-        <iframe src="{{ asset('storage/'.$outreach->outreach_report) }}" width="100%" height="1000px"></iframe>
+        <iframe src="{{ asset('proposal_outreach/storage/'.$outreach->outreach_report) }}" width="100%" height="1000px"></iframe>
     @endif
 </body>
 </html>
